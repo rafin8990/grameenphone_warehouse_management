6b53dd2ee8a9a4b8fb4eebafa6c9442670ffa360
rafin8990 <rafin8990@gmail.com>
2025-10-08 19:46:12 +0600

    module removed

--- Backend/src/app/routes/index.ts
+++ Backend/src/app/routes/index.ts
@@ -1,15 +1,10 @@
 import express from 'express';
-import { CategoryRoutes } from '../modules/categories/categories.route';
 import { DashboardRoutes } from '../modules/dashboard/dashboard.routes';
 import { ItemRoutes } from '../modules/items/items.route';
-import { LocationRoutes } from '../modules/locations/locations.route';
 import { PurchaseOrderRoutes } from '../modules/purchase-orders/purchase-orders.route';
 import { PoHexCodeRoutes } from '../modules/po-hex-codes/po-hex-codes.route';
 import { InboundRoutes } from '../modules/inbound/inbound.route';
-import { RequisitionRoutes } from '../modules/requisitions/requisitions.route';
-import { RfidRoutes } from '../modules/rfid/rfid.route';
-import { StockBalanceRoutes } from '../modules/stock-balance/stock-balance.route';
-import { VendorRoutes } from '../modules/vendors/vendors.route';
+
 
 const router = express.Router();
 
@@ -19,28 +14,8 @@ const moduleRoutes = [
     routes: DashboardRoutes,
   },
   {
-    path: '/categories',
-    routes: CategoryRoutes,
-  },
-  {
-    path: '/locations',
-    routes: LocationRoutes,
-  },
-  {
     path: '/items',
     routes: ItemRoutes,
-  },
-  {
-    path: '/vendors',
-    routes: VendorRoutes,
-  },
-  {
-    path: '/requisitions',
-    routes: RequisitionRoutes,
-  },
-  {
-    path: '/rfid',
-    routes: RfidRoutes,
   },
   {
     path: '/purchase-orders',
@@ -53,10 +28,6 @@ const moduleRoutes = [
   {
     path: '/inbound',
     routes: InboundRoutes,
-  },
-  {
-    path: '/stock-balances',
-    routes: StockBalanceRoutes,
   },
 ];
 
